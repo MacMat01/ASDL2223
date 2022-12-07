3e27e93874c01961299236d4030766fe8cc1746b
macmat01 <matteo.machella01@gmail.com>
2022-12-07 18:07:49 +0100

    Did some implementation but to be fixed and optimized

--- es12/src/BFSVisitor.java
+++ es12/src/BFSVisitor.java
@@ -1,10 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Classe singoletto che fornisce lo schema generico di visita Breadth-First di
  * un grafo rappresentato da un oggetto di tipo Graph<L>.
- * 
- * @author Template: Luca Tesei, Implementazione: collettiva
  *
  * @param <L> le etichette dei nodi del grafo
+ * @author Template: Luca Tesei, Implementazione: collettiva
  */
 public class BFSVisitor<L> {
 
@@ -18,29 +20,78 @@ public class BFSVisitor<L> {
      * visitNode sul nodo. In questa classe il metodo non fa niente, basta
      * creare una sottoclasse e ridefinire il metodo per eseguire azioni
      * particolari.
-     * 
-     * @param g
-     *                   il grafo da visitare.
-     * @param source
-     *                   il nodo sorgente.
-     * @throws NullPointerException
-     *                                      se almeno un valore passato è null
-     * @throws IllegalArgumentException
-     *                                      se il nodo sorgente non appartiene
-     *                                      al grafo dato
+     *
+     * @param g      il grafo da visitare.
+     * @param source il nodo sorgente.
+     * @throws NullPointerException     se almeno un valore passato è null
+     * @throws IllegalArgumentException se il nodo sorgente non appartiene
+     *                                  al grafo dato
      */
     public void BFSVisit(Graph<L> g, GraphNode<L> source) {
-        // TODO implementare
+        //TODO implementare
         // NOTA: chiamare il metodo visitNode quando un nodo passa da grigio a nero
+        if (g == null || source == null) {
+            throw new NullPointerException();
+        }
+        if (!g.containsNode(source)) {
+            throw new IllegalArgumentException();
+        }
+        /*
+         * Inizializza il grafo e chiama la recBFS sui nodi in un ordine
+         * qualsiasi per calcolare la "foresta" BFS
+         */
+        for (GraphNode<L> n : g.getNodes()) {
+            n.setColor(GraphNode.COLOR_WHITE);
+            n.setIntegerDistance(Integer.MAX_VALUE);
+            n.setPrevious(null);
+        }
+        /*
+         * Inizializza il nodo sorgente
+         */
+        source.setColor(GraphNode.COLOR_GREY);
+        source.setIntegerDistance(0);
+        source.setPrevious(null);
+        /*
+         * Coda dei nodi da visitare
+         */
+        Queue<GraphNode<L>> q = new LinkedList<>();
+        q.add(source);
+        /*
+         * Visita in ampiezza
+         */
+        while (!q.isEmpty()) {
+            /*
+             * Estrae il primo nodo dalla coda
+             */
+            GraphNode<L> u = q.remove();
+            /*
+             * Visita tutti i nodi adiacenti a u
+             */
+            for (GraphNode<L> v : g.getAdjacentNodesOf(u)) {
+                /*
+                 * Se il nodo v non è stato visitato lo aggiunge alla coda
+                 */
+                if (v.getColor() == GraphNode.COLOR_WHITE) {
+                    v.setColor(GraphNode.COLOR_GREY);
+                    /*
+                     * La distanza del nodo v è la distanza del nodo u più 1
+                     */
+                    v.setIntegerDistance(u.getIntegerDistance() + 1);
+                    v.setPrevious(u);
+                    q.add(v);
+                }
+            }
+            u.setColor(GraphNode.COLOR_BLACK);
+            visitNode(u);
+        }
     }
 
     /**
      * Questo metodo, che di default non fa niente, viene chiamato su tutti i
      * nodi visitati durante la BFS quando i nodi passano da grigio a nero.
      * Ridefinire il metodo in una sottoclasse per effettuare azioni specifiche.
-     * 
-     * @param n
-     *              il nodo visitato
+     *
+     * @param n il nodo visitato
      */
     public void visitNode(GraphNode<L> n) {
         /*
--- es12/src/DFSVisitor.java
+++ es12/src/DFSVisitor.java
@@ -1,10 +1,9 @@
 /**
  * Classe singoletto che fornisce lo schema generico di visita Depth-First di
  * un grafo rappresentato da un oggetto di tipo Graph<L>.
- * 
- * @author Template: Luca Tesei, Implementazione: collettiva
  *
  * @param <L> le etichette dei nodi del grafo
+ * @author Template: Luca Tesei, Implementazione: collettiva
  */
 public class DFSVisitor<L> {
 
@@ -20,29 +19,65 @@ public class DFSVisitor<L> {
      * metodo visitNode sul nodo. In questa classe il metodo non fa niente,
      * basta creare una sottoclasse e ridefinire il metodo per eseguire azioni
      * particolari.
-     * 
-     * @param g
-     *              il grafo da visitare.
-     * @throws NullPointerException
-     *                                  se il grafo passato è null
+     *
+     * @param g il grafo da visitare.
+     * @throws NullPointerException se il grafo passato è null
      */
     public void DFSVisit(Graph<L> g) {
-        // TODO implementare
+        //TODO implementare
         // NOTA: inizializza il grafo e chiama la recDFS sui nodi in un ordine
         // qualsiasi per calcolare la "foresta" DFS
 
+        if (g == null) {
+            throw new NullPointerException();
+        }
+        /*
+         * Inizializza il grafo
+         */
+        for (GraphNode<L> n : g.getNodes()) {
+            n.setColor(GraphNode.COLOR_WHITE);
+            n.setIntegerDistance(Integer.MAX_VALUE);
+            n.setPrevious(null);
+        }
+        /*
+         * Visita in profondità
+         */
+        time = 0;
+        for (GraphNode<L> n : g.getNodes()) {
+            if (n.getColor() == GraphNode.COLOR_WHITE) {
+                recDFS(g, n);
+            }
+        }
     }
 
     /*
      * Esegue la DFS ricorsivamente sul nodo passato.
-     * 
+     *
      * @param g il grafo
-     * 
+     *
      * @param u il nodo su cui parte la DFS
      */
     protected void recDFS(Graph<L> g, GraphNode<L> u) {
-        // TODO implementare ricorsivamente
+        //TODO implementare ricorsivamente
         // NOTA: chiamare il metodo visitNode alla "scoperta" di un nuovo nodo
+
+        u.setColor(GraphNode.COLOR_GREY);
+        visitNode(u);
+        /*
+         * Visita ricorsiva dei nodi adiacenti
+         */
+        for (GraphNode<L> v : g.getAdjacentNodesOf(u)) {
+            if (v.getColor() == GraphNode.COLOR_WHITE) {
+                v.setPrevious(u);
+                recDFS(g, v);
+            }
+        }
+        /*
+         * Visita terminata
+         */
+        u.setColor(GraphNode.COLOR_BLACK);
+        time++;
+        u.setIntegerDistance(time);
     }
 
     /**
@@ -50,9 +85,8 @@ public class DFSVisitor<L> {
      * nodi visitati durante la DFS nel momento in cui il colore passa da grigio
      * a nero. Ridefinire il metodo in una sottoclasse per effettuare azioni
      * specifiche.
-     * 
-     * @param n
-     *              il nodo visitato
+     *
+     * @param n il nodo visitato
      */
     public void visitNode(GraphNode<L> n) {
         /*
