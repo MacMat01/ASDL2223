325f92996a9b4c9257419c53c925190d37a8f156
macmat <matteo.machella01@gmailcom>
2022-12-07 11:45:04 +0100

    es12 imported

--- es12/src/MapAdjacentListDirectedGraphTest.java
+++ es12/src/MapAdjacentListDirectedGraphTest.java
@@ -1,10 +1,9 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MapAdjacentListDirectedGraphTest {
 
